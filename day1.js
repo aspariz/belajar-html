0dc42bff07c8a9ed5fdf67dc28edb8c1ea463e4f
Livyathan9 <rizantaazfa@gmail.com>
2025-09-26 23:23:59 +0700

    pertemuan pertama

---
+++ day1.js
@@ -0,0 +1,10 @@
+console.log(document.title)
+
+document.title = 'ambatukam'
+console.log(document.title)
+let body = document.body
+body.append("hallo wod")
+
+const h1 = document.createElement('h1')
+h1.textContent = "ambatukam"
+body.append(h1)
---
+++ skrip.js
@@ -0,0 +1,7 @@
+const menu_ikon = document.getElementById("ikon");
+const menu_list = document.getElementById("list")
+
+menu_ikon.addEventListener("click", () => {
+    menu_list.classList.toggle("hide")
+    
+});
